4a8a5638a995a2c6f03c69d602d3b2de0b776d01
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-01-17 13:47:41 -0300

    setting up prisma orm

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,19 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-  return "Hello world!";
+app.get("/", async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
 });
 
-app.listen({
-  port: 3001,
-});
+app
+  .listen({
+    port: 3001,
+  })
+  .then(() => {
+    console.log("HTTP Server is running");
+  });
