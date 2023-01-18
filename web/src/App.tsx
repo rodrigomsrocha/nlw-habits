c14c4fe28f72d98ad14533391ca70cddcb8cb949
rodrigomsrocha <rodrigomtc82@gmail.com>
2023-01-17 21:52:05 -0300

    daily habits table component done

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
-import { Habit } from "./components/Habit";
 import { Header } from "./components/Header";
+import { SumaryTable } from "./components/SummaryTable";
 
 export function App() {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+        <SumaryTable />
       </div>
     </div>
   );
